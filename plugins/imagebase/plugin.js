078338017823ecf6834eb478ed13de61447a1378
Tomasz Jakut <vepomoc@gmail.com>
2017-12-30 18:12:53 +0100

    Rename _toggleCaption to _refreshCaption.

--- plugins/imagebase/plugin.js
+++ plugins/imagebase/plugin.js
@@ -251,7 +251,7 @@
 
 					for ( i in widgets ) {
 						if ( hasWidgetFeature( widgets[ i ], 'caption' ) ) {
-							widgets[ i ]._toggleCaption( evt.data.path.lastElement );
+							widgets[ i ]._refreshCaption( evt.data.path.lastElement );
 						}
 					}
 				} );
@@ -266,7 +266,7 @@
 					this.parts.caption = createCaption( this );
 				}
 
-				this._toggleCaption();
+				this._refreshCaption();
 			},
 
 			/**
@@ -277,7 +277,7 @@
 			 * @member CKEDITOR.plugins.imagebase.featuresDefinitions.caption
 			 * @param {CKEDITOR.dom.element} sender Element, which triggered `selectionChange` event
 			 */
-			_toggleCaption: function( sender ) {
+			_refreshCaption: function( sender ) {
 				var isFocused = getFocusedWidget( this.editor ) === this,
 					caption = this.parts.caption,
 					editable = this.editables.caption;
