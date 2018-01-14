30c4854ed5cf68f2ce64d677a8574abb43dc4179
Marek Lewandowski <m.lewandowski@cksource.com>
2018-01-14 12:49:04 +0100

    Tests: WIP extracted balloontoolbar related tests into a separate suite. Added integration test for balloon position after clicking the caption and focusing back the widget.

--- tests/plugins/easyimage/commands.js
+++ tests/plugins/easyimage/commands.js
@@ -186,36 +186,6 @@
 
 					assert.areSame( 'side', widget.data.type, 'Widget has correct type data' );
 				} );
-			},
-
-			'test balloontoolbar integration': function( editor, bot ) {
-				bot.setData( widgetHtml, function() {
-					var widget = editor.widgets.getByElement( editor.editable().findOne( 'figure' ) ),
-						toolbar = editor.balloonToolbars._contexts[ 0 ].toolbar;
-
-					toolbar._view.once( 'show', function() {
-						assertCommandsState( editor, {
-							easyimageFull: CKEDITOR.TRISTATE_ON,
-							easyimageSide: CKEDITOR.TRISTATE_OFF,
-							easyimageAlt: CKEDITOR.TRISTATE_OFF
-						} );
-
-						editor.once( 'afterCommandExec', function() {
-							resume( function() {
-								assertCommandsState( editor, {
-									easyimageFull: CKEDITOR.TRISTATE_OFF,
-									easyimageSide: CKEDITOR.TRISTATE_ON,
-									easyimageAlt: CKEDITOR.TRISTATE_OFF
-								} );
-							} );
-						} );
-
-						editor.execCommand( 'easyimageSide' );
-					} );
-
-					widget.focus();
-					wait();
-				} );
 			}
 		};
 
